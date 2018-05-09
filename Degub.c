4fafbee678770312bce8d2d5797e7388391ff746
Steven Borisko <ibstevieb123@gmail.com>
2018-05-09 19:44:50 -0400

    initial commit

---
+++ Degub.c
@@ -0,0 +1,38 @@
+#include "Degub.h"
+
+#define ANSI_REDBLD "\33[31;1m"
+#define ANSI_YLWBLD "\33[33;1m"
+#define ANSI_WHTBLD "\33[37;1m"
+#define ANSI_TXTRST "\33[0m"
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void error(char* msg, int code) {
+	#if DEGUB_ERROR_ON
+	pthread_mutex_lock(&mutex);
+	fprintf(stderr, ANSI_REDBLD "[ERROR]: " ANSI_TXTRST);
+	fprintf(stderr,"%s\n",msg);
+	fprintf(stderr,"\terror code: %d - %s\n",code,strerror(code));
+	pthread_mutex_unlock(&mutex);
+	#endif
+}
+
+void warn(char* msg, int code) {
+	#if DEGUB_WARN_ON
+	pthread_mutex_lock(&mutex);
+	fprintf(stderr, ANSI_YLWBLD " [WARN]: " ANSI_TXTRST);
+	fprintf(stderr,"%s\n",msg);
+	fprintf(stderr,"\twarning code: %d\n",code);
+	pthread_mutex_unlock(&mutex);
+	#endif
+}
+
+void info(char* msg, int code) {
+	#if DEGUB_INFO_ON
+	pthread_mutex_lock(&mutex);
+	fprintf(stderr, ANSI_WHTBLD " [INFO]: " ANSI_TXTRST);
+	fprintf(stderr,"%s\n",msg);
+	fprintf(stderr,"\tinformation code: %d\n",code);
+	pthread_mutex_unlock(&mutex);
+	#endif
+}
---
+++ Degub.h
@@ -0,0 +1,59 @@
+#ifndef DEGUB_H
+#define DEGUB_H
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <pthread.h>
+
+//----------------------------------------------------------------------------//
+// --- Macro Definitions ---
+//----------------------------------------------------------------------------//
+
+#define DEGUB_ERROR_ON 1
+#define DEGUB_WARN_ON 1
+#define DEGUB_INFO_ON 1
+
+//----------------------------------------------------------------------------//
+// --- Main Function Declarations ---
+//----------------------------------------------------------------------------//
+
+/*
+--- error ---
+
+msg (char*)
+	- Custom message to be printed out.
+code (int)
+	- This should be the errno set from the erroneous function call.
+	- This function will print the string associated with the errno.
+
+Print an error message to stderr
+*/
+void error(char* msg, int code);
+
+/*
+--- warn --- 
+
+msg (char*)
+	- Custom message to be printed out.
+code (int)
+	- Custom status code to be printed out along with the message.
+
+Print an error message to stderr
+*/
+void warn(char* msg, int code);
+
+/*
+--- info --- 
+
+msg (char*)
+	- Custom message to be printed out.
+code (int)
+	- Custom status code to be printed out along with the message.
+
+Print an error message to stderr
+*/
+void info(char* msg, int code);
+
+#endif
