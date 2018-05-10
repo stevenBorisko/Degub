3b9256da583359473752e46a81f5587c65e5662c
Steven Borisko <ibstevieb123@gmail.com>
2018-05-09 22:47:52 -0400

    Updated documentation

--- Degub.mk
+++ Degub.mk
@@ -1,3 +1,5 @@
+# make <Degub path>/Degub.o a prerequisite for your executable
+
 DEGUB_PATH := $(dir $(realpath $(lastword $(MAKEFILE_LIST))))
 $(DEGUB_PATH)Degub.o: $(DEGUB_PATH)Degub.c $(DEGUB_PATH)Degub.h
 	$(CC) -c $< -o $@ $(CFLAGS)
