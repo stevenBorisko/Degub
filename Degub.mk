5801890954438ecdb9db9780f47dc56417445e32
Steven Borisko <sborisk@clemson.edu>
2018-05-17 12:17:13 -0400

    Made makefile more pretty

--- Degub.mk
+++ Degub.mk
@@ -1,8 +1,15 @@
 # make <Degub path>/Degub.o a prerequisite for your executable
 
+# --- Definitions ---
+
 DEGUB_PATH := $(dir $(realpath $(lastword $(MAKEFILE_LIST))))
+
+# --- Rules ---
+
+# Main object
 $(DEGUB_PATH)Degub.o: $(DEGUB_PATH)Degub.c $(DEGUB_PATH)Degub.h
 	$(CC) -c $< -o $@ $(CFLAGS)
 
+# Clean
 DegubClean:
 	rm -f $(DEGUB_PATH)*.o
